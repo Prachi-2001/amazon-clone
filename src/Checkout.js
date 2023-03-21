53077a657591636a4a853d3ae13f06e4b5839a8d
Prachi-2001 <prachipachang164@gmail.com>
2023-03-21 16:21:12 +0530

    added basket product cart functionality

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 // import Checkout_Ad from "./images/checkout-ad.jpg" ;
+import { useStateValue } from './StateProvider';
+
+
 import Subtotal from "./Subtotal";
 function Checkout() {
+  const [{basket},dispatch] = useStateValue();
+
   return (
-    <div className='checkout'>
+    <div className="checkout">
       <div className="checkout--left">
-        <img className="checkout-ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668.jpg" alt="" />
-       
-        <div className="checkout--title"><h2>Your Shopping Basket</h2></div>
-      </div> 
-      {/* end of the left hand checkout */}
-        <div className="chekout--right">
-      {/* new component for right side subtotal is  created  */}
-            <Subtotal/>
+        <img
+          className="checkout-ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668.jpg"
+          alt=""
+        />
+
+        <div >
+          <h2 className="checkout--title">Your Shopping Basket</h2>
+            {basket.map(item => {
+              return <CheckoutProduct 
+                id = {item.id}
+                title={item.title}
+                rating = {item.rating}
+                price = {item.price} 
+                image={item.image}
+              />
+            })}
         </div>
+      </div>
+      {/* end of the left hand checkout */}
+      <div className="chekout--right">
+        {/* new component for right side subtotal is  created  */}
+        <Subtotal />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./CheckoutProduct.css"
+import { useStateValue } from './StateProvider';
 
-function CheckoutProduct() {
+function CheckoutProduct({id,image,title,price,rating}) {
+  const removeFromBasket = () => {
+    
+  }
+  const [{basket},dispatch] = useStateValue();
   return (
-    <div>
-      
+    <div className='checkoutProduct'>
+      <img className="checkoutproduct--image" src={image} alt="" />
+    <div className="checkoutproduct--info">
+      <p className='checkoutproduct--title'>{title}</p>
+    
+      <p className='chekoutproduct--price'> <small>$</small> <strong>{price}</strong></p>
+      <div className="checkoutproduct--rating">
+           {/* create an array of 5 size and map with stars */}
+            {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+      </div>
+      <button onClick={removeFromBasket}>Remove from Basket</button>
+      </div>
     </div>
   )
 }
